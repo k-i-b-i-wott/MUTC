24d1ceefe36b2c77022fa8782bdf8548950f3871
Kibiwott <bk.kibiwott@gmail.com>
2025-03-21 11:00:13 +0300

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+
+
+const Header = () => {
+  return (
+    <header>
+      
+    </header>
+  )
+}
+
+export default Header
+
+
+function HeaderNav() {
+  return (
+   <div>
+    hello
+   </div>
+  )
+}
