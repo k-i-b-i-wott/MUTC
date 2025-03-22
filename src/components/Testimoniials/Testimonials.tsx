9b1f9981d38f5ace969e1e9caa8a9b527bdc1067
Kibiwott <bk.kibiwott@gmail.com>
2025-03-23 01:56:32 +0300

    Completed the testimonials section

---
+++ src/components/Testimoniials/Testimonials.tsx
@@ -0,0 +1,77 @@
+
+import './Testimonials.css'
+import doe from '../../assets/about.jpeg'
+
+
+interface testimonies{
+    name: string
+    role: string
+    description: string
+    image: string
+}
+const Testimonials = () => {
+    const testimony:testimonies[]=[
+        {
+            image: doe,
+            name: "John Doe",
+            role: "SoftwareEngineer at Google",
+            description: "MUTC played a crucial role in my tech journey. The hands-on coding sessions and networking events helped me gain real-world experience and land my dream job!"
+        },
+        {
+          image: doe,
+          name: "Brian Tanui",
+          role: "Machine Learning Engineer",
+          description:"I joined MUTC with no prior coding experience, and now I’m building AI models! This club truly empowers its members to excel in the ever-evolving world of technology."
+        },
+       {
+        image: doe,
+        name: "Jane Kelly",
+        role: "UI/UX Designer",
+        description:"The design workshops at MUTC helped me transition into UI/UX. The mentorship and support I received were invaluable",
+       },
+
+       {
+        image:doe,
+        name: "Alice Smith",
+        role: "Frontend Developer",
+        description:"MUTC provided me with the tools and resources to become a successful frontend developer. The hands-on projects and mentorship were invaluable!"
+
+       },
+       {
+        image:doe,
+        name: "Bob Johnson",
+        role: "Backend Developer",
+        description:"MUTC provided me with the tools and resources to become a successful backend developer. The hands-on projects and mentorship were invaluable!"
+
+       },
+       {
+        image:doe,
+        name: "Charlie Brown",
+        role: "Fullstack Developer",
+        description:"MUTC provided me with the tools and resources to become a successful fullstack developer. The hands-on projects, hackanthons , coding challenges and mentorship were invaluable!"
+
+       },
+
+
+    ]
+  return (
+    <div className='testimonials'>
+      {
+        testimony.map((testimonies,index) => (
+            <div key={index} className="testimony">
+                <div className="image-wrapper">
+                   <img src={testimonies.image} alt="images" />
+                </div>
+                <div className="testimony-wrapper">
+                <h4>{testimonies.name}</h4>
+                <h5>{testimonies.role}</h5>
+                <p>{testimonies.description}</p>
+                </div>
+            </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default Testimonials
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 
 import { About } from '../../components/About/About'
 import Hero from '../../components/Hero/Hero'
+import Testimonials from '../../components/Testimoniials/Testimonials'
 const Home = () => {
   return (
     <div>
       <Hero />
       <About  />
+      <Testimonials />
     </div>
   )
 }
