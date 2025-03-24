0c18972f6901d41ee3a7e0b97b7e0dfa35b3e881
Kibiwott <bk.kibiwott@gmail.com>
2025-03-24 11:39:24 +0300

    Styled the leadership section

--- src/pages/Leadership/Leadership.tsx
+++ src/pages/Leadership/Leadership.tsx
@@ -1,9 +1,16 @@
 import './Leadership.css'
 import image from '../../assets/alice.jpeg'
+import { FaFacebookF } from "react-icons/fa";
 const Leadership = () => {
   return (
-    <div>
-      <LeadershipTeam />  
+    <div className="lead-section">
+      <h1>Meet the Team</h1>
+      <div className='leadership'>
+          <LeadershipTeam />  
+          <LeadershipTeam />   
+          <LeadershipTeam />   
+          <LeadershipTeam />        
+    </div>
     </div>
   )
 }
@@ -12,24 +19,22 @@ export default Leadership
 
 function LeadershipTeam() {
   return (
-    <div className="leadership-section">
-      <div className="title">
-        <h5>Meet the team in charge</h5>
+    <div className="leadership-section">    
+      <div className='team-lead-image'>
+        <img src={image} alt="Lead" />
       </div>
-      <div className="team-lead">    
-        <div className="lead-image">  
-                  <img src={image} alt="Image" />  
-        </div>      
-        <div className="desc">
-          <h3>Name</h3>  
-          <h4>Role</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil expedita recusandae quam consequuntur consequatur, illo vero animi velit facilis nemo maxime explicabo veritatis, ratione dolorum nobis voluptatibus ab, est deleniti?</p>
-          <div className="social-links">
-            fafafaf
-          </div>
-        </div>  
-      </div>        
-      
+      <div className='desc'>
+        <h2>Name</h2>      
+        <h3>Role</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, atque.</p>
+        <div className="social-icons">
+          <FaFacebookF />
+          <FaFacebookF />
+          <FaFacebookF />
+          <FaFacebookF />
+        </div>
+      </div>  
+    
     </div>
   )
 }
