c8b9d94bebbff3c5905a9a3c9415cc7e1da40b05
Kibiwott <bk.kibiwott@gmail.com>
2025-03-25 21:40:36 +0300

    Added all the Events

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -1,47 +1,111 @@
+import { ReactNode } from "react";
+
 import { FcGlobe } from "react-icons/fc";
 import { CiClock1 } from "react-icons/ci";
+import { MdSecurity,MdPhoneIphone} from "react-icons/md";
+import { IoCodeDownload } from "react-icons/io5";
+import { TbCloudCog } from "react-icons/tb";
+import { LuMonitor } from "react-icons/lu";
+
 import './Events.css'
 
-// interface events {
-//     nameIcon: string
-//     name: string
-//     day: string
-//     time: string
-//     timeIcon: string
-//     location: string
-//     instructor: string
-//     classname: string
+interface events {
+    nameIcon:ReactNode
+    name: string
+    day: string
+    time: string
     
-// }
+    location: string
+    instructor: string
+    
+    
+}
 
 
 const Events = () => {
+    const eventDetails: events[] = [
+        {
+            nameIcon: [<MdSecurity />],
+            name: "Cyber Security",
+            day: "Every Monday",
+            time: "4:00PM - 7:00PM",
+            location: "Computer Lab F04",
+            instructor: "Webster Ifedha",
+            
+        },
+        {
+            nameIcon: [<IoCodeDownload />],
+            name: "UI/UX Design",
+            day: "Every Tuesday",
+            time:"5:00 PM - 8:00 PM",
+            location: "Computer Lab F04",
+            instructor: "Manase Gunga"
+        },
+        {
+            nameIcon: [<FcGlobe />],
+            name: "Web Development",
+            day: "Every Wednesday",
+            time:"6:00 PM - 9:00 PM",
+            location: "Computer Lab F04",
+            instructor: "Carolyne Githenduka"
+        },
+        {
+            nameIcon: [<MdPhoneIphone />],
+            name: "Mobile Apps Development",
+            day: "Every Thursday",
+            time:"4:00 PM - 7:00 PM",
+            location: "Computer Lab F04",
+            instructor: "Stanley Amunze"
+
+        },
+        {
+            nameIcon: [<TbCloudCog />],
+            name: "Cloud Engineering",
+            day: "Every Friday",
+            time:"3:00 PM - 6:00 PM",
+            location: "Computer Lab F04",
+            instructor: "Paul Karanja"
+
+        },
+        {
+            nameIcon: [<LuMonitor />],
+            name: " Power Platform",
+            day: "Every Saturday",
+            time:"1:00 PM - 4:00 PM",
+            instructor: "Evyonn Mbithe",
+            location: "Computer Lab F04"
+
+        }
+    ]
   return (
     <div className='events-section'>
         <h2>MUTC Weekly Activities</h2>
         <p>Empowering Tech Learning, One Workshop at a Time</p>
         <div className="events-container">
-            <div className="events-card">
+            {eventDetails.map((event, index) => (
+                <div key={index} className="events-card">
                 <div className='title'>
                     <div className="titleIcon">
-                          <FcGlobe />
+                          {event.nameIcon}
                     </div>
                     <div className="titleDesc">
-                        <h2>    Web  Development</h2>
-                        <h5>Every Monday</h5>
+                        <h2> {event.name}</h2>
+                        <h5>{event.day}</h5>
                     </div>
                 </div>
                 <div className='time'>
                     <h4><CiClock1 /></h4>                
-                    <h5>7:30PM - 9:00PM</h5>    
+                    <h5>{event.time}</h5>    
                 </div>
                 <div className='ints'>
-                    <p><strong>Instructor:</strong>Carolyne Githenduka</p>
+                    <p><strong>Instructor:</strong>{event.instructor}</p>
                 </div>
                 <div className='location'>
-                    <p><strong>Location:</strong>Room 1</p>
+                    <p><strong>Location:</strong>{event.location}</p>
                 </div>
-            </div>
+                </div>
+                
+            ))}           
         
         </div>      
     </div>
@@ -49,3 +113,5 @@ const Events = () => {
 }
 
 export default Events
+
+
